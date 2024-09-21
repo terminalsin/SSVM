12e1df1d158c8a487622f4deb85d794d6ef11f9e
EpicPlayerA10 <adasko.lesiak@gmail.com>
2024-09-21 13:17:42 +0200

    editorconfig

--- ssvm-core/src/main/java/dev/xdark/ssvm/execution/WrappedVMException.java
+++ ssvm-core/src/main/java/dev/xdark/ssvm/execution/WrappedVMException.java
@@ -4,17 +4,17 @@ package dev.xdark.ssvm.execution;
  * A wrapped {@link VMException} that will print much prettier and readable exception
  */
 public class WrappedVMException extends Throwable {
-  private final String className;
+	private final String className;
 
-  public WrappedVMException(String className, String message) {
-    super(message);
-    this.className = className;
-  }
+	public WrappedVMException(String className, String message) {
+		super(message);
+		this.className = className;
+	}
 
-  @Override
-  public String toString() {
-    String s = this.className; // Set correct class name
-    String message = getLocalizedMessage();
-    return (message != null) ? (s + ": " + message) : s;
-  }
+	@Override
+	public String toString() {
+		String s = this.className; // Set correct class name
+		String message = getLocalizedMessage();
+		return (message != null) ? (s + ": " + message) : s;
+	}
 }
